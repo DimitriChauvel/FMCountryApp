12781e5eba5481f0eeb790e9cf2a43440d9a0caf
Dimitri Chauvel <dimitri@pop-os.localdomain>
2023-05-16 07:36:15 +0200

    Fetch all countries data from REST Country

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,20 @@
 <template>
-  <main></main>
+  <main>
+    <pre v-for="country in countries" v-bind:key="country.name.common">
+      country: <span>{{ country.name.common }}</span>
+      region: <span>{{ country.region }}</span>
+      capital: <span v-for="capital in country.capital" v-bind:key="capital">{{ capital }}</span>
+      flag-url: <a v-bind:href="country.flags.svg">{{ country.flags.alt }}</a>
+    </pre>
+  </main>
 </template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const countries = ref([])
+
+  fetch('https://restcountries.com/v3.1/all')
+    .then((response) => response.json())
+    .then((data) => countries.value = data)
+</script>
